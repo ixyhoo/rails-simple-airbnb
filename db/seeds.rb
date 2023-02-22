281728e438987f557386adfe12798a0235ce19b3
jawad karroumi <ixyhoo@outlook.fr>
2023-02-22 16:56:08 +0100

    rails-simple-airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Nice & Spacious Garden Flat Paris',
+  address: '56 rue des parisien 102 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 500,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Spain',
+  address: '20 Gambas Gardens Spain W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat New York',
+  address: '102 Obama New York W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 96,
+  number_of_guests: 3
+)
